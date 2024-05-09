05dab247b7bf8cf8f722558144b6623c8ab4550b
FerreT-2157935 <125285581+grotekneus@users.noreply.github.com>
2024-05-09 13:34:59 +0200

    commit voor merge

--- ros2-webots-v2/build/ros2-webots-v2/prefix_override/sitecustomize.py
+++ ros2-webots-v2/build/ros2-webots-v2/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/seppe/Documents/Bachelor3/BRICK-WEBots/ros2-webots-v2/install/ros2-webots-v2'
+sys.prefix = sys.exec_prefix = '/home/ferre/brick/BRICK-WEBots/ros2-webots-v2/install/ros2-webots-v2'
--- ros2-webots-v2/install/ros2-webots-v2/share/ros2-webots-v2/package.sh
+++ ros2-webots-v2/install/ros2-webots-v2/share/ros2-webots-v2/package.sh
@@ -52,7 +52,7 @@ _colcon_prepend_unique_value() {
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/seppe/Documents/Bachelor3/BRICK-WEBots/ros2-webots-v2/install/ros2-webots-v2"
+_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/ferre/brick/BRICK-WEBots/ros2-webots-v2/install/ros2-webots-v2"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_package_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_package_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
